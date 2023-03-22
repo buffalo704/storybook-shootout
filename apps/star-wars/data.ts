05592dfff390915116d603c3e1e5e5988c7096cc
dixonmyles <dixon.myles@gmail.com>
2023-03-22 15:41:47 -0400

    Added Star Wars movie data

--- apps/star-wars/data.ts
+++ apps/star-wars/data.ts
@@ -100,3 +100,96 @@ export const people = [
     gender: 'male',
   },
 ]
+
+export const movies = [
+  {
+    id: '1',
+    name: 'Star Wars: Episode I - The Phantom Menace',
+    releaseDate: 'May 19, 1999',
+    genres: ['Action', 'Adventure', 'Fantasy'],
+    imdbRating: '6.5',
+    overview: 'Two Jedi escape a hostile blockade to find allies and come across a young boy who may bring balance to the Force, but the long dormant Sith resurface to claim their original glory.',
+    runtime: '2h 16m',
+    director: 'George Lucas'
+  },
+  {
+    id: '2',
+    name: 'Star Wars: Episode II - Attack of the Clones',
+    releaseDate: 'May 12, 2002',
+    genres: ['Action', 'Adventure', 'Fantasy'],
+    imdbRating: '6.6',
+    overview: 'Ten years after initially meeting, Anakin Skywalker shares a forbidden romance with Padmé Amidala, while Obi-Wan Kenobi discovers a secret clone army crafted for the Jedi.',
+    runtime: '2h 22m',
+    director: 'George Lucas'
+  },
+  {
+    id: '3',
+    name: 'Star Wars: Episode III - Revenge of the Sith',
+    releaseDate: '',
+    genres: ['Action', 'Adventure', 'Fantasy'],
+    imdbRating: '7.6',
+    overview: 'Three years into the Clone Wars, Obi-Wan pursues a new threat, while Anakin is lured by Chancellor Palpatine into a sinister plot to rule the galaxy.',
+    runtime: '2h 20m',
+    director: 'George Lucas',
+  },
+  {
+    id: '4',
+    name: 'Star Wars: Episode IV - A New Hope',
+    releaseDate: '',
+    genres: ['Action', 'Adventure', 'Fantasy'],
+    imdbRating: '8.6',
+    overview: 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire\'s world- destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.',
+    runtime: '2h 1m',
+    director: 'George Lucas',
+  },
+  {
+    id: '5',
+    name: 'Star Wars: Episode V - The Empire Strikes Back',
+    releaseDate: '',
+    genres: ['Action', 'Adventure', 'Fantasy'],
+    imdbRating: '8.7',
+    overview: 'After the Rebels are overpowered by the Empire, Luke Skywalker begins Jedi training with Yoda, while his friends are pursued across the galaxy by Darth Vader and bounty hunter Boba Fett.',
+    runtime: '2h 4m',
+    director: 'Irvin Kershner',
+  },
+  {
+    id: '6',
+    name: 'Star Wars: Episode VI - Return of the Jedi',
+    releaseDate: '',
+    genres: ['Action', 'Adventure', 'Fantasy'],
+    imdbRating: '8.3',
+    overview: 'After rescuing Han Solo from Jabba the Hutt, the Rebels attempt to destroy the second Death Star, while Luke struggles to help Darth Vader back from the dark side.',
+    runtime: '2h 11m',
+    director: 'Richard Marquand'
+  },
+  {
+    id: '7',
+    name: 'Star Wars: Episode VII - The Force Awakens',
+    releaseDate: '',
+    genres: ['Action', 'Adventure', 'Sci-Fi'],
+    imdbRating: '7.8',
+    overview: 'As a new threat to the galaxy rises, Rey, a desert scavenger, and Finn, an ex-stormtrooper, must join Han Solo and Chewbacca to search for the one hope of restoring peace.',
+    runtime: '2h 18m',
+    director: 'J.J. Abrams'
+  },
+  {
+    id: '8',
+    name: 'Star Wars: Episode VIII - The Last Jedi',
+    releaseDate: '',
+    genres: ['Action', 'Adventure', 'Fantasy'],
+    imdbRating: '6.9',
+    overview: 'The Star Wars saga continues as new heroes and galactic legends go on an epic adventure, unlocking mysteries of the Force and shocking revelations of the past.',
+    runtime: '2h 32m',
+    director: 'Rian Johnson'
+  },
+  {
+    id: '9',
+    name: 'Star Wars: Episode IX - The Rise of Skywalker',
+    releaseDate: '',
+    genres: ['Action', 'Adventure', 'Fantasy'],
+    imdbRating: '6.5',
+    overview: 'In the riveting conclusion of the landmark Skywalker saga, new legends will be born-and the final battle for freedom is yet to come.',
+    runtime: '2h 21m',
+    director: 'J.J. Abrams'
+  }
+]
